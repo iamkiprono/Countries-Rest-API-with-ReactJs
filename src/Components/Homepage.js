24c339c98d6da31941823b91c7d86fe6d01f8c5a
Collins Kiprono Kirui <101986591+iamkiprono@users.noreply.github.com>
2023-05-12 13:04:13 +0300

    Update Homepage.js

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -6,52 +6,51 @@ const Homepage = () => {
   const [search, setSearch] = useState("");
   const [filterQuery, setFilterQuery] = useState("");
   const [errorMessage, setErrorMessage] = useState(""); // new state for err message
-const getCountries = async () => {
-   try {
+  const getCountries = async () => {
+    try {
       const res = await fetch("https://restcountries.com/v3.1/all");
-      if(res.ok){
-      const data = await res.json();
-       setCountries(data);
-       setErrorMessage(""); //Clear the error message if the fetch is succesfull
-      }else{
-        setErrorMessage('Error fething countries');
-
-      }     
-     
+      if (res.ok) {
+        const data = await res.json();
+        setCountries(data);
+        setErrorMessage(""); //Clear the error message if the fetch is succesfull
+      } else {
+        setErrorMessage("Error fething countries");
+      }
     } catch (error) {
-      setErrorMessage('Error fething countries'); 
-    } 
-  
-}
-  
+      setErrorMessage("Error fething countries");
+    }
+  };
 
-useEffect(()=> {
-  getCountries()
-},[])
+  useEffect(() => {
+    getCountries();
+  }, []);
 
   return (
     <div className="homepage">
       <nav>
         <h3>Where in the world?</h3>
-        
       </nav>
       <div className="search-bar">
-        <input type="search" placeholder="Search for a country..." onChange={(e)=>setSearch(e.target.value)} />
+        <input
+          type="search"
+          placeholder="Search for a country..."
+          onChange={(e) => setSearch(e.target.value)}
+        />
         <div className="dropdown">
-        <label for="region">Filter by Region</label>
-        <select onChange={(e) => setFilterQuery(e.target.value)}>
-          <option value="">All</option>
-          <option value="africa">Africa</option>
-          <option value="america">America</option>
-          <option value="asia">Asia</option>
-          <option value="europe">Europe</option>
-          <option value="oceania">Oceania</option>
-        </select>
+          <label for="region">Filter by Region</label>
+          <select onChange={(e) => setFilterQuery(e.target.value)}>
+            <option value="">All</option>
+            <option value="africa">Africa</option>
+            <option value="america">America</option>
+            <option value="asia">Asia</option>
+            <option value="europe">Europe</option>
+            <option value="oceania">Oceania</option>
+          </select>
         </div>
       </div>
 
       {/* error message  */}
-        {errorMessage && (
+      {errorMessage && (
         <div className="error-box">
           <p className="error-message">{errorMessage}</p>
         </div>
@@ -61,7 +60,10 @@ useEffect(()=> {
         {countries
           .filter((country) =>
             country.name.common.toLowerCase().includes(search.toLowerCase())
-          ).filter((country) => country.region.toLowerCase().includes(filterQuery.toLowerCase()))
+          )
+          .filter((country) =>
+            country.region.toLowerCase().includes(filterQuery.toLowerCase())
+          )
           .map((country) => {
             return (
               <div className="country-card" key={country.name.common}>
